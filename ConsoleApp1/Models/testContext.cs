edc77562a61c0c22ab884a7d7b682ebe1559c6ed
Andrew <Andrew@DESKTOP-S1RPIPS>
2021-01-05 14:59:09 -0500

    aaa

---
+++ ConsoleApp1/Models/testContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ConsoleApp1.Models
+{
+    public partial class testContext : DbContext
+    {
+        public testContext()
+        {
+        }
+
+        public testContext(DbContextOptions<testContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.;Database=test;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.ToTable("Person");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Dob)
+                    .HasColumnType("date")
+                    .HasColumnName("DOB");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .HasColumnName("firstName")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .HasColumnName("lastName")
+                    .IsFixedLength(true);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new testContext())
+            {
+                //var andrew = new Person
+                //{
+                //    Dob = new DateTime(1973, 7, 3),
+                //    EyeColor = "Brown",
+                //    FirstName = "Andrew",
+                //    LastName = "Richardson"
+                //};
+
+                //context.People.Add(andrew);
+
+                //var amy = new Person
+                //{
+                //    Dob = new DateTime(1972, 7, 26),
+                //    EyeColor = "Blue",
+                //    FirstName = "Amy",
+                //    LastName = "Helton"
+                //};
+
+                //context.People.Add(amy);
+
+                var andrew = context.People.Where(x => x.FirstName.ToLower() == "andrew").FirstOrDefault();
+
+                var amy = context.People.Where(x => x.FirstName.ToLower() == "amy").FirstOrDefault();
+
+
+                Console.WriteLine($"{andrew.FirstName.Trim()} {andrew.LastName.Trim()}");
+                Console.WriteLine($"{amy.FirstName.Trim()} {amy.LastName.Trim()}");
+
+
+
+
+                context.SaveChanges();
+            }
+
+         }
+    }
+}
